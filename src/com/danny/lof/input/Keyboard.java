9721b21b60e7826e70576674f26057fca6e517d2
TenGumik <tengumik@gmail.cz>
2013-08-20 19:51:49 +0200

    Update Keyboard.java

--- src/com/danny/lof/input/Keyboard.java
+++ src/com/danny/lof/input/Keyboard.java
@@ -14,7 +14,7 @@ public class Keyboard implements KeyListener {
 		left = keys[KeyEvent.VK_LEFT] || keys[KeyEvent.VK_A];
 		right = keys[KeyEvent.VK_RIGHT] || keys[KeyEvent.VK_D];
 		f3 = keys[KeyEvent.VK_F3];
-		//Ahoj
+	
 	}
 
 	public void keyPressed(KeyEvent e) {
